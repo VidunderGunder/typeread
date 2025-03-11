ec3dc66154413d7a891c95fd621ddc5d03c81c92
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-11 13:48:00 +0100

    Refactor Logo component for improved styling and layout; enhance text visibility and shadow effects

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,23 +7,23 @@ export type LogoProps = {
 
 export function Logo({ className, ...props }: LogoProps) {
 	return (
-		<div
-			className={cn("relative flex items-center gap-2", className)}
-			{...props}
-		>
-			<img src="/android-chrome-192x192.png" alt="Logo" className="size-10" />
-			<h1 className="font-black text-2xl">
-				<span className="text-white">Type</span>
-				<span className="text-red-400">Read</span>
-			</h1>
+		<div className={cn("drop-shadow-lg", className)} {...props}>
+			<div className="relative flex items-center gap-2 drop-shadow-sm">
+				<img src="/android-chrome-192x192.png" alt="Logo" className="size-10" />
 
-			<span className="absolute bottom-0 left-[0px] size-0 w-max text-center text-white/50">
-				<p className="font-black text-[12.9px] leading-[1.2em]">
-					Improve typing speed
-					<br />
-					while enjoying a book
-				</p>
-			</span>
+				<h1 className="font-black text-2xl">
+					<span className="text-white">Type</span>
+					<span className="text-red-400">Read</span>
+				</h1>
+
+				<span className="absolute bottom-0 left-[0px] size-0 w-max text-center text-white/50">
+					<p className="text-[14.6px] leading-[1.2em]">
+						Improve typing speed
+						<br />
+						while enjoying a book
+					</p>
+				</span>
+			</div>
 		</div>
 	);
 }
