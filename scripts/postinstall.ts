af15521a444db3a12fdbe79dc2d88c3325997bc0
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-17 12:15:20 +0100

    Enhance logging in postinstall script by adding an emoji prefix to the go mod tidy command

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,7 +1,7 @@
 if (!process.env.VERCEL_ENV) {
 	import("node:child_process").then(({ execSync }) => {
 		const cmd = "cd backend && go mod tidy";
-		console.log(cmd);
+		console.log("🤖", cmd);
 		execSync(cmd, { stdio: "inherit" });
 	});
 }
