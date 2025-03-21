7054af01f5d80a3252dd9d7ce87b0ff643934cbc
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-21 23:03:43 +0100

    Optimize Hotkeys

--- src/components/Hotkeys.tsx
+++ src/components/Hotkeys.tsx
@@ -18,81 +18,124 @@ export type HotkeysProps = {
 	//
 } & Omit<ComponentProps<"div">, "children">;
 
-export function Hotkeys({ className, ...props }: HotkeysProps) {
-	const mode = useAtomValue(modeAtom);
-	const problemWords = useAtomValue(problemWordsAtom);
-	const { init, retry } = useInit();
-	const { toggle } = useFullscreen();
-	const [disableTyper] = useAtom(disableTyperAtom);
-	const typer = useTyperState();
-
+export function Hotkeys(props: HotkeysProps) {
 	return (
-		<div className={cn("", className)} {...props}>
+		<div {...props}>
 			<CommandsWrapper vertical>
-				<Glow glow={typer === "finished"}>
-					<Command
-						className={cn(typer === "finished" && "text-white")}
-						flip
-						disabled={disableTyper}
-						keyboard_key="Enter"
-						label="Next"
-						handler={(event) => {
-							event?.preventDefault();
-							if (mode === "book") {
-								init({
-									direction: "next",
-								});
-								return;
-							}
-							init({
-								problemWords,
-							});
-						}}
-					/>
-				</Glow>
-				{mode === "book" && (
-					<Command
-						flip
-						keyboard_key="KeyZ"
-						modifiers={[mod]}
-						label="Back"
-						disabled={mode !== "book" || disableTyper}
-						handler={(event) => {
-							event?.preventDefault();
-							init({
-								direction: "back",
-							});
-						}}
-					/>
-				)}
-				<Command
-					flip
-					disabled={disableTyper}
-					keyboard_key="KeyR"
-					modifiers={[mod]}
-					label="Retry"
-					handler={(event) => {
-						event?.preventDefault();
-						retry();
-					}}
-				/>
-				<Command
-					flip
-					disabled={disableTyper}
-					modifiers={[mod]}
-					keyboard_key="KeyI"
-					label="Definition"
-				/>
-				<Command
-					flip
-					modifiers={[mod]}
-					keyboard_key="KeyF"
-					handler={toggle}
-					label="Fullscreen"
-				/>
+				<NextCommand />
+				<BackCommand />
+				<RetryCommand />
+				<DefinitionCommand />
+				<FullscreenCommand />
 			</CommandsWrapper>
 			<hr className="my-2 text-white/25" />
 			<Profile />
 		</div>
+	);
+}
+
+function NextCommand() {
+	const mode = useAtomValue(modeAtom);
+	const problemWords = useAtomValue(problemWordsAtom);
+	const { init } = useInit();
+	const [disableTyper] = useAtom(disableTyperAtom);
+	const typer = useTyperState();
+
+	return (
+		<Glow glow={typer === "finished"}>
+			<Command
+				className={cn(typer === "finished" && "text-white")}
+				flip
+				disabled={disableTyper}
+				keyboard_key="Enter"
+				label="Next"
+				handler={(event) => {
+					event?.preventDefault();
+					if (mode === "book") {
+						init({
+							direction: "next",
+						});
+						return;
+					}
+					init({
+						problemWords,
+					});
+				}}
+			/>
+		</Glow>
+	);
+}
+
+function BackCommand() {
+	const mode = useAtomValue(modeAtom);
+	const { init } = useInit();
+	const [disableTyper] = useAtom(disableTyperAtom);
+
+	if (mode !== "book") return null;
+
+	return (
+		<Command
+			flip
+			keyboard_key="KeyZ"
+			modifiers={[mod]}
+			label="Back"
+			disabled={mode !== "book" || disableTyper}
+			handler={(event) => {
+				event?.preventDefault();
+				init({
+					direction: "back",
+				});
+			}}
+		/>
+	);
+}
+
+function RetryCommand() {
+	const mode = useAtomValue(modeAtom);
+	const { retry } = useInit();
+	const [disableTyper] = useAtom(disableTyperAtom);
+
+	if (mode !== "book") return null;
+
+	return (
+		<Command
+			flip
+			disabled={disableTyper}
+			keyboard_key="KeyR"
+			modifiers={[mod]}
+			label="Retry"
+			handler={(event) => {
+				event?.preventDefault();
+				retry();
+			}}
+		/>
+	);
+}
+
+function DefinitionCommand() {
+	const [disableTyper] = useAtom(disableTyperAtom);
+
+	return (
+		<Command
+			flip
+			disabled={disableTyper}
+			modifiers={[mod]}
+			keyboard_key="KeyI"
+			label="Definition"
+		/>
+	);
+}
+
+function FullscreenCommand() {
+	const { toggle } = useFullscreen();
+
+	return (
+		<Command
+			flip
+			modifiers={[mod]}
+			keyboard_key="KeyF"
+			handler={toggle}
+			label="Fullscreen"
+		/>
 	);
 }
