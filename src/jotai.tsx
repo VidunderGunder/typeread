7504ebdb2370f031988c7e8c14e6c18380aea376
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-04-03 00:46:17 +0200

    Update wallpaperAtom type to use WallpaperKey and change default value to 'fireplace'

--- src/jotai.tsx
+++ src/jotai.tsx
@@ -13,6 +13,7 @@ import { modeMap } from "./utils/constants";
 import { getBackIndex, getNextIndex } from "./utils/book";
 import { booksDefault } from "./default";
 import { processEpub } from "./utils/file";
+import type { WallpaperKey } from "./components/Wallpaper";
 
 // ASCII Text Generator:
 // https://patorjk.com/software/taag/#p=display&f=Elite&t=Hello%20World
@@ -61,7 +62,10 @@ export const wordsPerChunks = [10, 25, 50, 100] as const;
 export type WordsPerChunk = (typeof wordsPerChunks)[number];
 export const wordsPerChunkAtom = atomWithStorage<WordsPerChunk>("amount", 25);
 
-export const wallpaperAtom = atomWithStorage<string>("wallpaper", "mushrooms");
+export const wallpaperAtom = atomWithStorage<WallpaperKey>(
+	"wallpaper",
+	"fireplace",
+);
 
 export const wpmAtom = atomWithReset<number>(0);
 export const charsAtom = atom<Character[]>([]);
